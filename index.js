1ce233b9e422643db65623354964bcad1c897e67
paulzzz16 <allanpaultabuada@gmail.com>
2022-12-24 22:39:35 +0800

    add sample search

--- index.js
+++ index.js
@@ -4,13 +4,30 @@ function initMap() {
     const cebu = { lat: 10.3156992, lng: 123.88543660000005 };
     // The map, centered at Uluru
     const map = new google.maps.Map(document.getElementById("map"), {
-      zoom: 4,
+      zoom: 15,
       center: cebu,
     });
     // The marker, positioned at cebu
     const marker = new google.maps.Marker({
       position: cebu,
       map: map,
+    });
+
+
+    var request = {
+      query: 'restaurant',
+      fields: ['restaurant'],
+    };
+  
+    var service = new google.maps.places.PlacesService(map);
+  
+    service.findPlaceFromQuery(request, function(results, status) {
+      if (status === google.maps.places.PlacesServiceStatus.OK) {
+        for (var i = 0; i < results.length; i++) {
+          createMarker(results[i]);
+        }
+        map.setCenter(results[0].geometry.location);
+      }
     });
   }
   
